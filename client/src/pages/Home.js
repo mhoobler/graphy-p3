0cd7bc09c936f62557361898faf19d0f69eda027
Michael Hoobler <hoobler.michael@gmail.com>
2019-06-26 06:39:22 -0400

    changed Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,20 +8,8 @@ class Home extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                </header>
+                <h2>Welcome to Graphy</h2>
+                <p>(under construction)</p>
             </div>
         )
     }
